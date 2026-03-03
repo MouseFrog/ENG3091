29860ad7ac832cf725f59e8cf3c98e0dce747bf6
MouseFrog <kluilin108@gmail.com>
2026-03-03 15:40:51 +0000

    created DataGenerator class, with data generating functions tgt for more object oriented coding

--- Task_2/multi_data.cpp
+++ Task_2/multi_data.cpp
@@ -19,24 +19,70 @@ struct ValuesConfig {
 };
 
 // Generate random values for variables
+double getRandomValue(std::mt19937& mt_num, double min, double max)
+
+// Fill up matrices based on random generated data and equations
+// Access ValuesConfig for pre-set equation parameters
+void generate_data(std::vector<std::vector<double>>& var_matrix, 
+                           std::vector<double>& price_matrix, 
+                           const ValuesConfig& config, // Access values from ValuesConfig
+                           std::mt19937& mt_num)
+
+
+// Adds noise to individual data point
+void calc_noise(std::vector <double>& data_in, std::mt19937& mt_num)
+
+// Transpose matrix
+std::vector<std::vector<double>> transposeMatrix(const std::vector<std::vector<double>>& matrix_in)
+
+// Iterate through matrix and save into .csv file
+// Matrix is read only to preserve original
+void saveFile(const std::vector<std::vector<double>>& data_in, std::string file_name)
+
+int main() {
+
+    // Set up random data generation
+    std::random_device device_num; // Random number generated from hardware 
+    std::mt19937 mt_num(device_num()); // Mersenne Twister pseudo-random generator 
+
+    // Setup matrix for storing value of variables
+    //int data_points = 100;  // row: # of data points
+    //int num_variables = 3; // column: y-intercept, land size, # of bedrooms
+    std::vector<std::vector<double>>  multi_data(100, std::vector<double>(3, 0.0)); // 100x3 matrix, initialise all points with value of 0.0
+    std::vector<std::vector<double>>  single_data(100, std::vector<double>(2, 0.0)); // 100x2 matrix, initialise all points with value of 0.0
+
+    // Initialise vector for holding house prices
+    std::vector <double> single_price(100); // Simple Linear Regression
+    std::vector <double> multi_price(100); // Multi Linear Regression
+
+    // Set up ValuesConfig Object for value parameters
+    ValuesConfig data_config;
+
+
+    generate_data(single_data, single_price, data_config,mt_num);
+    generate_data(multi_data, multi_price, data_config,mt_num);
+
+    // Add noise to house prices
+    calc_noise(single_price, mt_num);
+    calc_noise(multi_price, mt_num);
+
+    saveFile(multi_data,"og file" );
+
+    std::vector<std::vector<double>> transposed_multi= transposeMatrix(multi_data);
+    saveFile(transposed_multi,"transposed file");
+    
+    return 0;
+}    
+    
+    /*
+// Generate random values for variables
 double getRandomValue(std::mt19937& mt_num, double min, double max){
     std::uniform_int_distribution<> variable_dist(min,max);
     double variable_val = variable_dist(mt_num);
     return variable_val;
-}
+}*/
 
-// Adds noise to each individual data point
-void calc_noise(std::vector <double>& data_in, std::mt19937& mt_num){ // Pass by reference alters original dataset, return nothing
-    double current_sum = 0;
-    for (int i=0; i<data_in.size(); i++){
-        double indiv_dev = data_in[i] * 0.05;   // Calc standard deviation for each point to apply noise
-        // Gaussian White Noise
-        std::normal_distribution<double> noise_dist(0.0, indiv_dev);
-        double noise = noise_dist(mt_num);
-        data_in[i]=data_in[i]+noise;
-    }
-}
-
+/*
 // Fill up matrices based on random generated data and equations
 // Access ValuesConfig for pre-set equation parameters
 void generate_data(std::vector<std::vector<double>>& var_matrix, 
@@ -52,7 +98,6 @@ void generate_data(std::vector<std::vector<double>>& var_matrix,
 
         // Fill up matrix column with respective values
         const double intercept = 1.0;
-
         var_matrix[i][0] = intercept; // Reserved for intercept and math manipulation later on
         var_matrix[i][1] = bedroom;
 
@@ -66,7 +111,19 @@ void generate_data(std::vector<std::vector<double>>& var_matrix,
         price_matrix[i] = config.w_intercept + (config.w_bedroom * bedroom);  }
         
     }
-}
+}*/
+/*
+// Adds noise to each individual data point
+void calc_noise(std::vector <double>& data_in, std::mt19937& mt_num){ // Pass by reference alters original dataset, return nothing
+    double current_sum = 0;
+    for (int i=0; i<data_in.size(); i++){
+        double indiv_dev = data_in[i] * 0.05;   // Calc standard deviation for each point to apply noise
+        // Gaussian White Noise
+        std::normal_distribution<double> noise_dist(0.0, indiv_dev);
+        double noise = noise_dist(mt_num);
+        data_in[i]=data_in[i]+noise;
+    }
+}*/
 
 // Transpose matrix
 std::vector<std::vector<double>> transposeMatrix(const std::vector<std::vector<double>>& matrix_in){
@@ -127,43 +184,65 @@ void saveFile(const std::vector<std::vector<double>>& data_in, std::string file_
     std::cerr << "Unable to open file!" << std::endl; // Raise error if unable to open file
     } 
 } 
-    
 
+// DATA GENERATION
+class DataGenerator {
 
-int main() {
+// Only the functions inside this class can see these variables
+private:
+    std::mt19937 mt_num;
+    ValuesConfig config;
 
-    // Set up random data generation
-    std::random_device device_num; // Random number generated from hardware 
-    std::mt19937 mt_num(device_num()); // Mersenne Twister pseudo-random generator 
+public:
+    // CONSTRUCTOR: This is where you put the "setup" instructions.
+    DataGenerator(ValuesConfig c) : config(c) {
+        std::random_device device_num;
+        mt_num.seed(device_num()); 
+    }
 
-    // Setup matrix for storing value of variables
-    //int data_points = 100;  // row: # of data points
-    //int num_variables = 3; // column: y-intercept, land size, # of bedrooms
-    std::vector<std::vector<double>>  multi_data(100, std::vector<double>(3, 0.0)); // 100x3 matrix, initialise all points with value of 0.0
-    std::vector<std::vector<double>>  single_data(100, std::vector<double>(2, 0.0)); // 100x2 matrix, initialise all points with value of 0.0
+    void generate_data(std::vector<std::vector<double>>& var_matrix, 
+                           std::vector<double>& price_matrix) {  
 
-    // Initialise vector for holding house prices
-    std::vector <double> single_price(100); // Simple Linear Regression
-    std::vector <double> multi_price(100); // Multi Linear Regression
+    for (int i = 0; i < var_matrix.size(); i++) {
 
-    // Set up ValuesConfig Object for value parameters
-    ValuesConfig data_config;
+        // Generate random variable values
+        double bedroom = getRandomValue(mt_num, config.bed_min, config.bed_max);
+        double area = getRandomValue(mt_num, config.area_min, config.area_max);
 
+        // Fill up matrix column with respective values
+        const double intercept = 1.0;
+        var_matrix[i][0] = intercept; // Reserved for intercept and math manipulation later on
+        var_matrix[i][1] = bedroom;
 
-    generate_data(single_data, single_price, data_config,mt_num);
-    generate_data(multi_data, multi_price, data_config,mt_num);
+        // Multi-Varible Calculations
+        if (var_matrix[i].size() >1){   //Check if matrix is multi-variable
+        var_matrix[i][2] = area;
+        price_matrix[i] = config.w_intercept + (config.w_bedroom * bedroom) + (config.w_area * area);
 
-    // Add noise to house prices
-    calc_noise(single_price, mt_num);
-    calc_noise(multi_price, mt_num);
+        }
+        else {//Single Variable Calculations
+        price_matrix[i] = config.w_intercept + (config.w_bedroom * bedroom);  }
+        
+    }
+}
 
-    saveFile(multi_data,"og file" );
+    // Generate random values for variables
+    double getRandomValue(double min, double max){
+    std::uniform_int_distribution<> variable_dist(min,max);
+    double variable_val = variable_dist(mt_num);    // mt_num is accessible here
+    return variable_val;
+    }
 
-    std::vector<std::vector<double>> transposed_multi= transposeMatrix(multi_data);
-    saveFile(transposed_multi,"transposed file");
-    
-    return 0;
-}    
-    
-    
-    
+    // Adds noise to each individual data point
+    void calc_noise(std::vector <double>& data_in){ // Pass by reference alters original dataset, return nothing
+        double current_sum = 0;
+        for (int i=0; i<data_in.size(); i++){
+            double indiv_dev = data_in[i] * 0.05;   // Calc standard deviation for each point to apply noise
+            // Gaussian White Noise
+            std::normal_distribution<double> noise_dist(0.0, indiv_dev);
+            double noise = noise_dist(mt_num);  // mt_num is accessible here
+            data_in[i]=data_in[i]+noise;
+        }
+    }
+
+};
