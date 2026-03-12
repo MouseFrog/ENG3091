2ba0519e77af3f2bd020f0d1813c1709185d2090
MouseFrog <kluilin108@gmail.com>
2026-03-12 21:56:42 +0000

    code from 'task_2' incorported into file structure, edited to be compatible with data generated from DataGenerator

--- Task_2/GradientDescent.cpp
+++ Task_2/GradientDescent.cpp
@@ -1,13 +1,17 @@
 #include "MultiRegression.hpp"
 
-//Multiple Linear Regression
+// This file consists Gradient Descent class 
 GradientDescent::GradientDescent(int numFeatures, double lr ) : learningRate(lr) {    
         // Vector size: # of variables + Intercept 
         // Resized within constructor so it isn't altered accidentally, causing size mismatch 
         weights.resize(numFeatures + 1, 0.0); 
     }
 
-void GradientDescent::train(const std::vector<std::vector<double>>& X, const std::vector<double>& Y, int iteration) {
+
+void GradientDescent::train(const std::vector<std::vector<double>>& X, 
+                            const std::vector<std::vector<double>>& Y, 
+                            int iteration) {
+
     int m = X.size();    // Number of samples, row
     int n = X[0].size(); // # of weights including intercept
 
@@ -21,15 +25,16 @@ void GradientDescent::train(const std::vector<std::vector<double>>& X, const std
                 y_prediction += weights[j] * X[i][j]; // addition of all weight*x-val in row i
             }
             // Error in predicted y
-            double error = y_prediction - Y[i];  
+            double error = y_prediction - Y[i][0];  
 
             for (int j = 0; j < n; j++) {
                 gradients[j] += error * X[i][j]; // Calculate errors proportional to each data point
             }
         }
         // Update weights using the average gradient
+        // Take one step size away from calculated error
         for (int j = 0; j < n; j++) {
-            weights[j] -= (learningRate * gradients[j]) / m;    // Take one step size away from calculated error
+            weights[j] -= (learningRate * gradients[j]) * (2.0/m);    // 2.0 to force floating pt calculations
         }
     }
 }
