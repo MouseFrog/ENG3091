d33abeb192a54d1a9f6287bac37a2a79fa12b60a
MouseFrog <kluilin108@gmail.com>
2026-03-26 14:42:32 +0000

    added converge threshold to track when weight calculations have stabilised

--- Task_2/GradientDescent.cpp
+++ Task_2/GradientDescent.cpp
@@ -1,12 +1,12 @@
+// This file contains the implementation of the Gradient Descent method
+
 #include "MultiRegression.hpp"
 
-// This file consists Gradient Descent class 
 GradientDescent::GradientDescent(int numFeatures, double lr ) : learningRate(lr) {    
         // Vector size: # of variables + Intercept 
         // Resized within constructor so it isn't altered accidentally, causing size mismatch 
         weights.resize(numFeatures + 1, 0.0); 
     }
-
 
 void GradientDescent::train(const std::vector<std::vector<double>>& X, 
                             const std::vector<std::vector<double>>& Y, 
@@ -16,7 +16,16 @@ void GradientDescent::train(const std::vector<std::vector<double>>& X,
     int n = X[0].size(); // # of weights including intercept, col
 
     for (int e = 0; e < iteration; e++) {  // # of iterations
+
+        // Value beyond which change is negligible
+        double converge_threshold = 1*10e-12;
+
+        // Gradient vector to store error in each data point
+        // Reset every iteration
         std::vector<double> gradients(n, 0.0);  
+
+        // Store current weights to compare change in weights for this iteration
+        std::vector<double> old_weights = weights;
 
         for (int i = 0; i < m; i++) {
 
@@ -38,6 +47,19 @@ void GradientDescent::train(const std::vector<std::vector<double>>& X,
         // Takes one step size away from calculated error
         for (int j = 0; j < n; j++) {
             weights[j] -= (learningRate * gradients[j]) * (2.0/m);    // 2.0 to force floating pt. calculations
+        }
+
+        // Track magnitude of change between old and new weights
+        double overall_change = 0.0;
+        for (int j = 0; j < n; j++){
+            double weight_change = old_weights[j]-weights[j];
+            overall_change +=  weight_change * weight_change;
+        }
+        
+        overall_change = std::sqrt(overall_change);
+        if (overall_change < converge_threshold){
+            std::cout<<"Iteration complete: "<< e <<"\n";
+            break;
         }
     }
 }
