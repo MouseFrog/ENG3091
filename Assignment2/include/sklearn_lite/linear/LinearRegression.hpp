49d63d16c36685bf1c246f3e5774712b7675619a
MouseFrog <kluilin108@gmail.com>
2026-04-18 10:44:06 +0100

    copy pasted 3 models into respective hpp files, in casethalternative file organisation is preferred

---
+++ Assignment2/include/sklearn_lite/linear/LinearRegression.hpp
@@ -0,0 +1,18 @@
+#ifndef SKLEARN_LITE_LINEARREGRESSION_HPP
+#define SKLEARN_LITE_LINEARREGRESSION_HPP
+
+#include <iostream>
+#include <vector>   // for std::vector
+#include "Models.hpp"
+
+namespace sklearn_lite::linear {
+
+class LinearRegression {
+
+
+   
+};
+
+} 
+
+#endif 
---
+++ Assignment2/include/sklearn_lite/linear/LogisticRegression.hpp
@@ -0,0 +1,19 @@
+#ifndef SKLEARN_LITE_LOSGISTICREGRESSION_HPP
+#define SKLEARN_LITE_LOSGISTICREGRESSION_HPP
+
+#include <iostream>
+#include <vector>   // for std::vector
+#include "Models.hpp"
+
+
+namespace sklearn_lite::linear {
+
+class LogisticRegression : public {
+
+
+   
+};
+
+} 
+
+#endif 
